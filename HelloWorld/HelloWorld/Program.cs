23e06bfa3580a4e0bd8c7236087ea8fb85e1f3df
Jacob Harris <jake98harris@gmail.com>
2019-01-15 14:01:05 -0600

    JAHAINDWRK-3 Spelling Fix

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Henlo WaaARLD");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
