1b75cdb4aa22c1c33a61378eb6c24b916be198ef
Jacob Harris <jake98harris@gmail.com>
2019-02-08 14:02:39 -0600

    Loading works

--- SGBank/SGBank.Data/FileAccountRepository.cs
+++ SGBank/SGBank.Data/FileAccountRepository.cs
@@ -12,9 +12,11 @@ namespace SGBank.Data
     public class FileAccountRepository : IAccountRepository
     {
         private Dictionary<string, Account> _accounts;
+        private readonly string _path;
         public FileAccountRepository(string path)
         {
-            string[] rows = File.ReadAllLines(path);
+            _path = path;
+            string[] rows = File.ReadAllLines(_path);
             _accounts = new Dictionary<string, Account>();
             foreach (string row in rows)
             {
@@ -24,12 +26,34 @@ namespace SGBank.Data
         }
         public Account LoadAccount(string AccountNumber)
         {
-            throw new NotImplementedException();
+            if(_accounts.ContainsKey(AccountNumber))
+            {
+                return _accounts[AccountNumber];
+            }
+            return null;
         }
 
         public void SaveAccount(Account account)
         {
-            throw new NotImplementedException();
+            if(!_accounts.ContainsKey(account.AccountNumber))
+            {
+                throw new KeyNotFoundException("The account number has been changed since last load. Please contact IT");
+            }
+            _accounts[account.AccountNumber] = account;
+            //SaveFile();
+        }
+        private List<Account> ReadAllAccounts()
+        {
+            return _accounts.Values.ToList();
+        }
+        private void SaveFile()
+        {
+            List<string> accountData = new List<string>();
+            foreach (Account account in ReadAllAccounts())
+            {
+                accountData.Add(FileAccountHandler.ToRow(account));
+            }
+            File.WriteAllLines(_path, accountData);
         }
     }
 }
