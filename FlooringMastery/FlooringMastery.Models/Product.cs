f3b080d3d4c18f1001635588236aa0fcaed86719
Jacob Harris <jake98harris@gmail.com>
2019-02-11 14:41:10 -0600

    Further framework, product struct

---
+++ FlooringMastery/FlooringMastery.Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Models
+{
+    public struct Product
+    {
+        public readonly string Name;
+        public readonly decimal CostPerSquareFoot;
+        public readonly decimal LaborCostPerSquareFoot;
+        public Product(string name, decimal costPSF, decimal laborCostPSF)
+        {
+            Name = name;
+            CostPerSquareFoot = costPSF;
+            LaborCostPerSquareFoot = laborCostPSF;
+        }
+    }
+}
