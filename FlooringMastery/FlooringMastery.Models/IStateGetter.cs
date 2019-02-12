f813d415303532c115e31fd3b1aec31bb11e546b
Jacob Harris <jake98harris@gmail.com>
2019-02-12 13:14:33 -0600

    Added Interfaces for minor noun "repositories"

---
+++ FlooringMastery/FlooringMastery.Models/IStateGetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Models
+{
+    public interface IStateGetter
+    {
+        State GetState(string id); // can use either name or abbrevation
+    }
+}
