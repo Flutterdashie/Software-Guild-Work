199d53111856c93d2bf42dffd5e3126cbfdf34d9
Jacob Harris <jake98harris@gmail.com>
2019-02-12 14:40:40 -0600

    added Product and State getter implementations. will work on default handling later.

---
+++ FlooringMastery/FlooringMastery.Data/ProductGetter.cs
@@ -0,0 +1,25 @@
+﻿using FlooringMastery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Data
+{
+    public class ProductGetter : IProductGetter
+    {
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return ProductMapper.GetAllProducts();
+        }
+
+        public Product GetProductByName(string name)
+        {
+            var query = from p in ProductMapper.GetAllProducts()
+                        where p.ProductType == name
+                        select p;
+            return query.FirstOrDefault();
+        }
+    }
+}
---
+++ FlooringMastery/FlooringMastery.Data/StateGetter.cs
@@ -0,0 +1,20 @@
+﻿using FlooringMastery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Data
+{
+    public class StateGetter : IStateGetter
+    {
+        public State GetState(string id)
+        {
+            var query = from s in StateMapper.GetStates()
+                        where s.StateAbbr == id || s.StateName == id
+                        select s;
+            return query.FirstOrDefault();
+        }
+    }
+}
