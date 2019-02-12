f79dca65ca79f3e3868ac78313344ca51c391f75
Jacob Harris <jake98harris@gmail.com>
2019-02-12 13:49:08 -0600

    Tax mapper complete!

--- FlooringMastery/FlooringMastery.Data/TaxMapper.cs
+++ FlooringMastery/FlooringMastery.Data/TaxMapper.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FlooringMastery.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace FlooringMastery.Data
 {
     public static class TaxMapper
     {
+        public static List<State> GetStates()
+        {
+            List<State> result = new List<State>();
+            string[] lines =File.ReadAllLines(@".\taxes.txt");
+            foreach(string line in lines)
+            {
+                string[] values = line.Split(',');
+                result.Add(new State(values[1], values[0], decimal.Parse(values[2])));
+            }
+            return result;
+        }
     }
 }
