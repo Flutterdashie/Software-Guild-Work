8409b99f75e965cc61c6fbeac0c360e26f031db5
Jacob Harris <jake98harris@gmail.com>
2019-02-13 09:25:40 -0600

    Sorta started Order Repo stuff

---
+++ FlooringMastery/FlooringMastery.Models/IOrderRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Models
+{
+    public interface IOrderRepo
+    {
+        IEnumerable<Order> GetOrdersByDate(DateTime date);
+        Order GetSpecificOrder(int orderNum, DateTime date);
+    }
+}
