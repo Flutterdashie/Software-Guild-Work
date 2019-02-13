750f66bab8d457384f2a2c021695a9a6e8ef8aaf
Jacob Harris <jake98harris@gmail.com>
2019-02-13 17:12:40 -0600

    First testing bit is working!

---
+++ FlooringMastery/FlooringMastery.Tests/DataTests.cs
@@ -0,0 +1,22 @@
+﻿using FlooringMastery.Data;
+using FlooringMastery.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Tests
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        public void SafelyThrowsForMissingEntry()
+        {
+            IOrderRepo orderRepo = new TestOrderRepo();
+            Assert.Throws(typeof(InvalidOperationException),new TestDelegate(() =>orderRepo.GetSpecificOrder(3, new DateTime(2013, 1, 1))));
+        }
+    }
+}
