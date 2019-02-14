eece8d627faade8d75e5972355d60577a9c9b878
Jacob Harris <jake98harris@gmail.com>
2019-02-14 08:33:17 -0600

    slight test modification

--- FlooringMastery/FlooringMastery.Tests/DataTests.cs
+++ FlooringMastery/FlooringMastery.Tests/DataTests.cs
@@ -12,11 +12,18 @@ namespace FlooringMastery.Tests
     [TestFixture]
     public class DataTests
     {
-        [Test]
-        public void SafelyThrowsForMissingEntry()
+        //[Test]
+        //public void SafelyThrowsForMissingEntry()
+        //{
+        //    IOrderRepo orderRepo = new TestOrderRepo();
+        //    Assert.Throws(typeof(InvalidOperationException),new TestDelegate(() =>orderRepo.GetSpecificOrder(3, new DateTime(2013, 1, 1))));
+        //}
+
+        [TestCase(3,2013,1,1)]
+        public void SafelyThrowsForMissingEntry(int orderNum, int year, int month, int day)
         {
             IOrderRepo orderRepo = new TestOrderRepo();
-            Assert.Throws(typeof(InvalidOperationException),new TestDelegate(() =>orderRepo.GetSpecificOrder(3, new DateTime(2013, 1, 1))));
+            Assert.Throws(typeof(InvalidOperationException), new TestDelegate(() => orderRepo.GetSpecificOrder(orderNum, new DateTime(year, month, day))));
         }
     }
 }
