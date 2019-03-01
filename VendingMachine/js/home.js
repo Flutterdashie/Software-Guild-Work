3e1abc9d16713ccfd6358e4688c034cfea618132
Jacob Harris <jake98harris@gmail.com>
2019-03-01 14:54:28 -0600

    Live item adding almost in place!

--- VendingMachine/js/home.js
+++ VendingMachine/js/home.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    //alert(convertToMoney($('#quarters'),$('#dimes'),$('#nickels'),$('#pennies')));
+});
+
+function convertToMoney(quarters,dimes,nickels,pennies) {
+    return ((parseInt(quarters.val()) * 25 + parseInt(dimes.val()) * 10 + parseInt(nickels.val()) * 5 + parseInt(pennies.val()) ) * 0.01).toFixed(2);
+}
 
 
 function buyItem(money, itemID) {
@@ -12,6 +19,31 @@ function buyItem(money, itemID) {
             422: function (xhr) {
                 alert($.parseJSON(xhr.responseText).message);
             }
+        }
+    });
+}
+
+function populateItems () {
+    'use strict';
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/items',
+        success: function (data) {
+            $('#itemBoxes').text('');
+            $.each(data, function(index,item) {
+                var itemDiv = '<div class="col-4" id="item';
+                itemDiv += item.id;
+                itemDiv += '">';
+                itemDiv += '<span class="text-left" style="font-size:8pt">';
+                itemDiv += item.id;
+                itemDiv += '</span>';
+                
+                itemDiv += "</div>";
+                $('#itemBoxes').append(itemDiv);
+            })
+        },
+        error: function () {
+            alert('no u');
         }
     });
 }
