17972baafa014233e6684ebf52f5170514cf4553
Jacob Harris <jake98harris@gmail.com>
2019-03-16 22:23:37 -0500

    ADO code complete, not yet tested or run

--- DvdLibraryAPI/DvdLibraryAPI/Models/Repositories/DvdRepositoryADO.cs
+++ DvdLibraryAPI/DvdLibraryAPI/Models/Repositories/DvdRepositoryADO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,28 @@ namespace DvdLibraryAPI.Models.Repositories
         }
         public Dvd Create(Dvd dvd)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "UpdateByID";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@title", dvd.title);
+                cmd.Parameters.AddWithValue("@realeaseYear", dvd.realeaseYear);
+                cmd.Parameters.AddWithValue("@director", dvd.director);
+                cmd.Parameters.AddWithValue("@rating", dvd.rating);
+                if (dvd.notes != null)
+                {
+                    cmd.Parameters.AddWithValue("@notes", dvd.notes);
+                }
+
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr).FirstOrDefault();
+                }
+            }
         }
 
         public void Delete(int id)
@@ -25,44 +47,144 @@ namespace DvdLibraryAPI.Models.Repositories
                 conn.ConnectionString = _connStr;
                 SqlCommand cmd = new SqlCommand();
                 cmd.CommandText = "DeleteByID";
+                cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Connection = conn;
-                throw new NotImplementedException();
+                cmd.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
             }
         }
 
         public Dvd Read(int id)
         {
-            throw new NotImplementedException();
+            //TODO: Ask Elijah/Amir/Corbin/Somebody about how efficient this is because I THINK this makes it only evaluate the first step of the query (which is all it can evaluate anyway) so I'm proud of it
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "GetByID";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr).FirstOrDefault();
+                }
+            }
         }
 
         public IEnumerable<Dvd> ReadAll()
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "GetAll";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr);
+                }
+            }
         }
 
         public IEnumerable<Dvd> ReadByDirector(string director)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "GetByDirector";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Search", director);
+                cmd.Connection = conn;
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr);
+                }
+            }
         }
 
         public IEnumerable<Dvd> ReadByRating(string rating)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "GetByRating";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Search", rating);
+                cmd.Connection = conn;
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr);
+                }
+            }
         }
 
         public IEnumerable<Dvd> ReadByTitle(string title)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "GetByTitle";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Search", title);
+                cmd.Connection = conn;
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr);
+                }
+            }
         }
 
         public IEnumerable<Dvd> ReadByYear(string year)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "GetByYear";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Search", year);
+                cmd.Connection = conn;
+                conn.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    return InterpretReader(dr);
+                }
+            }
         }
 
         public void Update(Dvd dvd)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = _connStr;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "UpdateByID";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@dvdId", dvd.dvdId);
+                cmd.Parameters.AddWithValue("@title", dvd.title);
+                cmd.Parameters.AddWithValue("@realeaseYear", dvd.realeaseYear);
+                cmd.Parameters.AddWithValue("@director", dvd.director);
+                cmd.Parameters.AddWithValue("@rating", dvd.rating);
+                if(dvd.notes != null)
+                {
+                    cmd.Parameters.AddWithValue("@notes", dvd.notes);
+                }
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
         }
 
         private static IEnumerable<Dvd> InterpretReader(SqlDataReader reader)
