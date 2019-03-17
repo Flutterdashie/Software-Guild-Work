ea6e76594d057ea3fc0dd9e69da027c2ff7e937c
Jacob Harris <jake98harris@gmail.com>
2019-03-16 23:15:58 -0500

    Getting Tests going

---
+++ DvdLibraryAPI/DvdLibraryAPI.Tests/ADOTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvdLibraryAPI.Tests
+{
+    [TestFixture]
+    public class ADOTests
+    {
+        [OneTimeSetUp]
+        [OneTimeTearDown]
+        public void ResetDatabase()
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Data Source=localhost;initial catalog=DvdLibrary;persist security info=True;user id=DvdLibraryApp;password=testing123;MultipleActiveResultSets=True;";
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "HardReset";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
